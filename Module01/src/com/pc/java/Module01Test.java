b7829c5da28c08ee900e067d8f9a08259bfacd86
Pc <pc@PcdeMacBook-Pro.local>
2020-08-05 20:44:54 +0800

    Initial commit

---
+++ Module01/src/com/pc/java/Module01Test.java
@@ -0,0 +1,19 @@
+package com.pc.java;
+
+import java.util.ArrayList;
+
+
+public class Module01Test {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        ArrayList list = new ArrayList<>();
+
+
+        //command + d 复制
+
+    }
+
+}
---
+++ Module01/src/com/pc/java/Test01.java
@@ -0,0 +1,8 @@
+package com.pc.java;
+
+/**
+ * @author Pc
+ * @create 2020-08-05 8:07 下午
+ */
+public class Test01 {
+}
